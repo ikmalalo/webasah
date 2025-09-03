afc1f1e3cfcafe71548f5d16a650104182aa8407
mpasd4.py <ikmalaliashari@gmail.com>
2025-09-03 23:48:41 +0800

    aa

--- js/home.js
+++ js/home.js
@@ -76,3 +76,10 @@
         document.querySelectorAll('.fade-in, .fade-in-delayed, .fade-in-slow, .popup-card').forEach(el => {
             observer.observe(el);
         });
+
+    const burger = document.getElementById('burger-menu');
+    const navLinks = document.getElementById('nav-links');
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
